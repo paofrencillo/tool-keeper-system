bdd5cf1b071b559bd93c3294f794bfe42ce00825
paopaolopaoloo <simonruzzel@gmail.com>
2022-09-08 11:06:15 +0800

    modified settings.py

--- tks_webapp/settings.py
+++ tks_webapp/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'tks/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tks/static/')
