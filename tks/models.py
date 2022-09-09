26a79f9fce85c2330732c7fcd4a0db5d3765356d
paopaolopaoloo <simonruzzel@gmail.com>
2022-09-09 12:18:21 +0800

    modified models.py

--- tks/models.py
+++ tks/models.py
@@ -4,26 +4,26 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     tupc_id = models.BigIntegerField(primary_key=True)
-    year_course = models.CharField()
-    role = models.CharField()
+    year_course = models.CharField(max_length=20)
+    role = models.CharField(max_length=11)
 
 
 class Tools(models.Model):
     tool_id = models.BigIntegerField(primary_key=True)
     tool_name = models.CharField(unique=True)
     tool_image = models.ImageField(upload_to='imgs/', null=False, blank=False)
-    storage = models.CharField()
-    layer = models.CharField()
-    status = models.CharField()
+    storage = models.CharField(max_length=10)
+    layer = models.CharField(max_length=8)
+    status = models.CharField(max_length=25)
 
 
 class Transactions(models.Model):
     borrower_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    fullname = models.CharField()
-    year_course = models.CharField()
+    fullname = models.CharField(max_length=255)
+    year_course = models.CharField(max_length=20)
     borrow_datetime = models.DateTimeField()
     return_datetime = models.DateTimeField()
-    status = models.CharField(default="AVAILABLE")
+    status = models.CharField(max_length=25, default="AVAILABLE")
 
 
 class ToolsBorrowed(models.Model):
