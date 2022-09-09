6ac71760f09b8ef1d9fc8130133545aec1b198f5
cefbok <xcef.1991@gmail.com>
2022-09-09 14:54:53 +0800

    registration

--- tks_webapp/settings.py
+++ tks_webapp/settings.py
@@ -134,10 +134,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'tks/media/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'tks/static/')
 ]
 
 # Default primary key field type
