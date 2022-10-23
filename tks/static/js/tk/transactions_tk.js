5851e7535da360567bc88219f06e49ab70e22568
paopaolopaoloo <simonruzzel@gmail.com>
2022-10-23 21:34:27 +0800

    add colors on status

--- tks/static/js/tk/transactions_tk.js
+++ tks/static/js/tk/transactions_tk.js
@@ -1,4 +1,17 @@
 $('#transactions-table').DataTable( {
   "paging": true,
   "searching": true
-} );
+});
+
+$(document).ready(function() {
+  var status = document.getElementsByClassName('status');
+  for ( const s of status ) {
+    if ( s.innerText == 'RESERVED' ) {
+      s.style.color = '#9C9C06';
+    } else if ( s.innerText == 'BORROWED' ) {
+      s.style.color = '#008000';
+    } else if ( s.innerText == '0000FF' ) {
+      s.style.color = '#9C9C06';
+    }
+  }
+});
