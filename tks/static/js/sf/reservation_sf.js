e49a1d98519cb2acbb516f6a653bc4376221d85a
paopaolopaoloo <simonruzzel@gmail.com>
2022-10-23 12:56:39 +0800

    a

---
+++ tks/static/js/sf/reservation_sf.js
@@ -0,0 +1,16 @@
+function checkDateTime() {
+    var borrow_date = document.getElementsByName('borrow-date')[0].value;
+    var borrow_time = document.getElementsByName('borrow-time')[0].value;
+    var return_date = document.getElementsByName('return-date')[0].value;
+    var return_time = document.getElementsByName('return-time')[0].value;
+    var submit_btn = document.getElementById('submit-btn');
+
+    var startTime = new Date(`${borrow_date} ${borrow_time}`);
+    var endTime = new Date(`${return_date} ${return_time}`);
+
+    if ( startTime >= endTime ) {
+        alert("start time is lesser");
+    } else if ( startTime < endTime ) {
+        alert("YES!")
+    }
+}
