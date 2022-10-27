3c12e30f0c277755266dfefa5f829a30430ce9d0
paopaolopaoloo <simonruzzel@gmail.com>
2022-10-27 18:23:33 +0800

    modified js for tool selection

--- tks/static/js/sf/home_sf.js
+++ tks/static/js/sf/home_sf.js
@@ -12,7 +12,7 @@ function getTools(element) {
     let tool_name = element.childNodes[1].innerText;
     let canvas = document.getElementById('tool-selected');
     canvas.innerHTML += `<div data-toolid="${tool_id}" class="selected-tools-wrapper input-group align-items-center mb-3 py-1">
-                        <input type="text" class="form-control selected-tools" value="${tool_id + '    ' + tool_name}" readonly disabled>
+                        <input type="text" class="form-control selected-tools" name="${tool_id}" value="${tool_id + '    ' + tool_name}" readonly disabled>
                         <div class="remove-btn" onclick="removeSelectedTool(this)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
