a3974d0b2ba24d8abe5c5835261e7b74f5cb590a
paopaolopaoloo <simonruzzel@gmail.com>
2023-01-19 11:10:20 +0800

    test deploy

--- tks_webapp/settings.py
+++ tks_webapp/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 DEFAULT_FILE_STORAGE = 'storages.backends.sftpstorage.SFTPStorage'
-SFTP_STORAGE_HOST = 'https://tkstupc.com'
+SFTP_STORAGE_HOST = 'tkstupc.com'
 SFTP_STORAGE_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
