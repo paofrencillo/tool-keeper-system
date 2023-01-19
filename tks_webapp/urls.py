c7d0a0d7d01f72dd8d088d2285a41a8d58024070
paopaolopaoloo <simonruzzel@gmail.com>
2023-01-19 11:43:17 +0800

    test deploy

--- tks_webapp/urls.py
+++ tks_webapp/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tks.urls')),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
