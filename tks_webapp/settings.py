2a04878f59773577f71a72ad5be93e15163c92e2
paopaolopaoloo <simonruzzel@gmail.com>
2023-01-19 11:14:06 +0800

    test deploy

--- tks_webapp/settings.py
+++ tks_webapp/settings.py
@@ -155,9 +155,9 @@ SFTP_STORAGE_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://tkstupc.com/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
